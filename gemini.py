df8411267c8c659eb48cccbffbfb1f2fb3c29611
juliacereja <jcerejameinhardt@gmail.com>
2026-05-15 11:00:28 -0300

    Update gemini.py

--- gemini.py
+++ gemini.py
@@ -5,17 +5,17 @@ genai.configure(api_key=st.secrets["GOOGLE_API_KEY"])
 
 model = genai.GenerativeModel("gemini-2.5-flash")
 
-def gerar_ideia():
+def responder_pergunta():
 
-    prompt = "Crie uma ideia, resumida a 2 ou 3 palavras, (que não esteja entre essas: pular de paraquedas, ler 20 livros, viajar de avião, visitar 5 cidades do Brasil, ter filhos, amar de verdade, fazer uma aula de dança, realizar um sonho com alguém, viajar para o exterior, cozinhar uma comida só para você, partivipar de um campeonato, ir ao show de um cantor que você ama), de meta para uma lista de coisas para fazer antes de morrer."
+    prompt = st.text_input
 
     resposta = model.generate_content(prompt)
 
     return resposta.text
 
-st.subheader("Gerar meta com IA!")
-if st.button("Gerar ideia com IA"):
+st.subheader("Faça uma pergunta a IA")
+if st.text_input("Digite sua pergunta:")
 
-    ideia = gerar_ideia()
+    ideia = responder_pergunta()
 
     st.success(ideia)
